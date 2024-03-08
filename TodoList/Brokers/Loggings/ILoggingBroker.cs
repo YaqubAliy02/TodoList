f9cc5b1070efcce3d47db0fb567ec046f925ad07
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-03-08 22:20:01 +0500

    Resolve Problem And Create Brokers Folder

---
+++ TodoList/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoList.Brokers.Logging
+{
+    internal interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogInformation(string message);
+        void LogError(string message);
+
+    }
+}
---
+++ TodoList/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TodoList.Brokers.Logging;
+
+namespace TodoList.Brokers.Loggings
+{
+    internal class LoggingBroker : ILoggingBroker
+    {
+        public void LogError(Exception exception)
+        {
+           Console.ForegroundColor = ConsoleColor.DarkRed;
+           Console.WriteLine(exception);
+           Console.ResetColor();
+        }
+
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public void LogInformation(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
